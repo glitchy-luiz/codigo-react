2b796b4ce782d1bbba7016c5cf8b269b55ce87b2
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-09-20 20:50:26 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom"
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <>
-            <header>
+            <header className="bg-purple-800 flex text-white justify-around h-14 items-center">
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-4">
                         <li><NavLink to='/'>Home</NavLink></li>
                         <li><NavLink to='filmes'>Filmes</NavLink></li>
                         <li><NavLink to='genero'>Gêneros</NavLink></li>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,13 @@
-export default function MovieCard() {
-    return(
+import { Link } from "react-router-dom";
+
+export default function MovieCard({ id, titulo, imagem_destaque }) {
+    return (
         <>
-        <h1>Movie Card</h1>
-        {/* Modelo de card que será utilizado para os filmes */}
+            <div>
+                <h2>{titulo}</h2>
+                <img src={imagem_destaque} alt={titulo} className="w-28 h-36" />
+                <Link to={`/filmes/${id}`}>Saber mais</Link>
+            </div>
         </>
     )
 
